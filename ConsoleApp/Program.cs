70cfb8cb1181411ab9cdc0c634259fddaa11fc77
Oussama Saibi <saibioussama@outlook.fr>
2018-06-10 13:41:38 +0100

    --

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -10,14 +10,11 @@ namespace ConsoleApp
     class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("begin ");
+        { 
             //test zone
-
             Employe e = new Employe() { Matr = 1,Poste = "post" ,NomE ="oussama",Salaire=1000,Comm = 10,DateEmb="10/10/2017",_Departement = new Departement() { NumDept=10},Emp_Sup = new Employe() { Matr = 1020} };
             Employe.InsertEmploye(e);
-            //end test zone
-            Console.WriteLine("end");
+            //end test zone 
             Console.ReadKey();
         }
     }
--- DAL/DbConnect.cs
+++ DAL/DbConnect.cs
@@ -15,7 +15,6 @@ namespace DAL
         public DbConnect(string _ConnectionString = @"Server= 127.0.0.1; port=3306;Database=myschema;Uid=root;Pwd=root;")
         {
             Connection = new MySqlConnection();
-            //Connection.ConnectionString = @"Database=to_do_item_azure_db;Data Source=eu-cdbr-azure-west-d.cloudapp.net;User Id=b1b3e01d198971;Password=1f01a988";
             Connection.ConnectionString = _ConnectionString;
             Connection.Open();
 
